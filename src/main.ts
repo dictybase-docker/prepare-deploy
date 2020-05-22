995a5ed6fdeacea82f1c24aa2972c76f0316de29
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-05-22 14:35:45 -0500

    feat:add  deployment request using octokit

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,28 @@ async function run(): Promise<void> {
       "ref",
       "token",
     ]
-    inputs.forEach(input => console.log(`${input} == ${core.getInput(input)}`))
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
+    const octokit = new github.GitHub(core.getInput("token"), {
+      required: true,
+    })
+    const { repo, owner } = github.context.repo
+    const ref = core.getInput("ref", { required: true })
+    const resp = octokit.repos.createDeployment({
+      owner: owner,
+      repo: repo,
+      ref: ref,
+      auto_merge: false,
+      required_contexts: [],
+      description: "deploy request from dictybaseBot",
+      payload: JSON.stringify({
+        cluster: core.getInput("cluster-name", { required: true }),
+        zone: core.getInput("cluster-zone", { required: true }),
+        chart: core.getInput("chart-name", { required: true }),
+        path: core.getInput("chart-path", { required: true }),
+        namespace: core.getInput("namespace", { required: true }),
+        image_tag: core.getInput("image-tag", { required: true }),
+      }),
+    })
+    octokit.log.info(`preapred deployment for ref ${ref} in ${owner}/${repo}`)
   } catch (error) {
     core.setFailed(error.message)
   }
