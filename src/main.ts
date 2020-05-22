21d9f247ddfb6404a8e0db8a18c3b9097e57332e
wildlifehexagon <crossthebreeze@gmail.com>
2020-05-22 10:14:31 -0500

    refactor: set up a simple script to test

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import * as core from "@actions/core"
-import { wait } from "./wait"
+import * as github from "@actions/github"
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput("milliseconds")
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput("time", new Date().toTimeString())
+    const inputs = [
+      "cluster-name",
+      "cluster-zone",
+      "chart-name",
+      "chart-path",
+      "namespace",
+      "image-repo",
+      "image-tag",
+    ]
+    inputs.forEach(input => console.log(`${input} == ${core.getInput(input)}`))
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
   } catch (error) {
     core.setFailed(error.message)
   }
